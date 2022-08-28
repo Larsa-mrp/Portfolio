94794eafe3f500bcb23df3eecfe55051d87b82fc
Bradley <bradleyspiclin@gmail.com>
2022-08-28 12:22:59 +1000

    email

--- function.js
+++
@@ -1,9 +0,0 @@
-// date object is declared as constant
-const d = new Date().toLocaleString();
-// variables for reference for canvas
-var cnv = document.getElementById("canvas");
-var ctx = cnv.getContext("2d");
-
-function showDate(){
-    document.getElementById('date').innerHTML= d;
-}
